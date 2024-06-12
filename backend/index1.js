328bc56888694a57b626b062ebefb43905853943
harshita-200 <harshitakedia29@gmail.com>
2024-06-12 19:49:42 +0530

    commit3

--- backend/index1.js
+++ backend/index1.js
@@ -14,6 +14,7 @@ app.use((req,res,next)=>{
   );
   next();
 })
+app.use(cors());
 app.use(express.json());
 app.use('/api',require("./routes/CreateUser"));
 app.use('/api',require("./routes/DisplayData"));
