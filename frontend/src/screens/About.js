3e461fa13257c17500eb8a46543b72f3908d4e0e
harshita-200 <harshitakedia29@gmail.com>
2024-06-15 10:40:31 +0530

    csschange

--- frontend/src/screens/About.js
+++ frontend/src/screens/About.js
@@ -24,12 +24,14 @@ const footerStyle = {
 
 export default function About() {
   return (
-    <div style={containerStyle}>
+    <div style={{backgroundColor:'black'}}>
+        <div style={containerStyle}>
       <Navbar />
       <div className="container" style={contentStyle}>
         Welcome to YumCourier, where we transform meal times into delightful experiences with a simple click. Founded in 2024 by a team of passionate food enthusiasts, we connect you with a diverse array of culinary delights from local favorites to popular chains, all delivered fresh and hot to your door. Our mission is to provide convenience, quality, and variety, ensuring every meal is a memorable one. With our user-friendly platform and dedicated customer service, we strive to make enjoying great food as easy and enjoyable as possible. Thank you for choosing YumCourier.
       </div>
       <Footer style={footerStyle} />
+    </div>
     </div>
   );
 }
