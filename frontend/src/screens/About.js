cfae19ef945f5b56ee2778af61b58d2e0679da45
harshita-200 <harshitakedia29@gmail.com>
2024-06-15 10:28:43 +0530

    footercss

--- frontend/src/screens/About.js
+++ frontend/src/screens/About.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
 
@@ -15,37 +15,21 @@ const contentStyle = {
   alignItems: 'center',
   fontSize: '1.5rem', // Increase font size
   textAlign: 'center', // Center align text
-  opacity: 0,
-  animation: 'slideIn 1s forwards' // Apply the animation
+  transition: 'all 0.5s ease', // Add transition effect
 };
 
-const About = () => {
-  useEffect(() => {
-    const styleSheet = document.styleSheets[0]; // Replace with correct index or reference
-    const keyframes = `
-      @keyframes slideIn {
-        0% {
-          transform: translateY(50%);
-          opacity: 0;
-        }
-        100% {
-          transform: translateY(0);
-          opacity: 1;
-        }
-      }
-    `;
-    styleSheet.insertRule(keyframes, styleSheet.cssRules.length);
-  }, []); // Run once after component mounts
+const footerStyle = {
+  flexShrink: 0, // Ensure footer doesn't shrink
+};
 
+export default function About() {
   return (
     <div style={containerStyle}>
       <Navbar />
       <div className="container" style={contentStyle}>
         Welcome to YumCourier, where we transform meal times into delightful experiences with a simple click. Founded in 2024 by a team of passionate food enthusiasts, we connect you with a diverse array of culinary delights from local favorites to popular chains, all delivered fresh and hot to your door. Our mission is to provide convenience, quality, and variety, ensuring every meal is a memorable one. With our user-friendly platform and dedicated customer service, we strive to make enjoying great food as easy and enjoyable as possible. Thank you for choosing YumCourier.
       </div>
-      <Footer style={{ flexShrink: 0 }} /> {/* Ensure Footer sticks to the bottom */}
+      <Footer style={footerStyle} />
     </div>
   );
-};
-
-export default About;
+}
