8a1c866f662723adfcf928a9d430a60042725501
harshita-200 <harshitakedia29@gmail.com>
2024-06-16 11:01:24 +0530

    commit

--- backend/routes/CreateUser.js
+++ backend/routes/CreateUser.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
-const bcrypt = require("bcrypt.js");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const jwtSecret = "MyNameIsHarshitaKediandIamwebDev";
 
