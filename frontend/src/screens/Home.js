1af00a0285ae86793b66a6bf05c6af53e655409a
harshita-200 <harshitakedia29@gmail.com>
2024-06-17 08:56:00 +0530

    commit

--- frontend/src/screens/Home.js
+++ frontend/src/screens/Home.js
@@ -65,7 +65,7 @@ export default function Home() {
 
           <div className="carousel-item">
             <img
-              src="https://media.istockphoto.com/id/184946701/photo/pizza.jpg?s=1024x1024&w=is&k=20&c=L8h3NUxinbmrBkSc7ETJHHxlEn6qYX4xzqIahI1Nh10="
+              src="https://media.istockphoto.com/id/184946701/photo/pizza.jpg?s=612x612&w=0&k=20&c=97rc0VIi-s3mn4xe4xDy9S-XJ_Ohbn92XaEMaiID_eY="
               className="img-fluid w-100"
               style={{ filter: "brightness(80%)" }}
               alt="..."
@@ -73,7 +73,7 @@ export default function Home() {
           </div>
           <div className="carousel-item">
             <img
-              src="https://imgs.search.brave.com/vxcXKFT32FnQ1o-_4FMz4dzXJmkNRhUnHYQwfcqh59w/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTA0/OTM2OTUzMC9waG90/by9wYXN0cnktcHVm/Zi1pbi1iYWtlcnku/anBnP3M9NjEyeDYx/MiZ3PTAmaz0yMCZj/PVp2WjlfR19nLUt5/VkM4enBJQkpFZFRt/bE9YQXd1M2NoWExl/a0pWUWxUOGs9"
+              src="https://live.staticflickr.com/4745/38718537835_584882a2ed_h.jpg"
               className="img-fluid w-100"
               style={{ filter: "brightness(80%)" }}
               alt="..."
